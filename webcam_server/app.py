789d2e4e3feb8f28de9faa1d6bbdcf5ac27942bc
Injae Shin <shininjae1213@naver.com>
2023-09-30 22:54:25 +0900

    Describe your changes here

--- webcam_server/app.py
+++ webcam_server/app.py
@@ -3,26 +3,26 @@ from flask_cors import CORS
 import cv2
 import numpy as np
 import base64
-import os
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
 # YOLO Configurations
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-YOLO_CFG = os.path.join(BASE_DIR, "webcam_server/yolov4-tiny.cfg")
-YOLO_WEIGHTS = os.path.join(BASE_DIR, "webcam_server/yolov4-tiny.weights")
-YOLO_CLASSES = os.path.join(BASE_DIR, "webcam_server/coco.names")
+YOLO_CFG = "yolov3.cfg"
+YOLO_WEIGHTS = "yolov3.weights"
+YOLO_CLASSES = "coco.names"
 CONFIDENCE_THRESHOLD = 0.5  # adjust to your needs
 NMS_THRESHOLD = 0.4  # adjust to your needs
 
 net = cv2.dnn.readNet(YOLO_WEIGHTS, YOLO_CFG)
 layer_names = net.getLayerNames()
 output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers().flatten()]
+
 classes = open(YOLO_CLASSES).read().strip().split("\n")
 
 def detect_objects(image):
     height, width, channels = image.shape
+
     blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
     net.setInput(blob)
     outs = net.forward(output_layers)
@@ -41,8 +41,10 @@ def detect_objects(image):
                 center_y = int(detection[1] * height)
                 w = int(detection[2] * width)
                 h = int(detection[3] * height)
+
                 x = int(center_x - w / 2)
                 y = int(center_y - h / 2)
+
                 boxes.append([x, y, w, h])
                 confidences.append(float(confidence))
                 class_ids.append(class_id)
@@ -61,14 +63,12 @@ def detect():
     if not image_data:
         return jsonify(error="no image provided"), 400
 
-    try:
-        # Decode the base64 image
-        image_data = base64.b64decode(image_data.split(",")[1])
-        image = cv2.imdecode(np.frombuffer(image_data, dtype=np.uint8), cv2.IMREAD_COLOR)
-        detected_objects = detect_objects(image)
-        return jsonify(detected_objects=detected_objects)
-    except Exception as e:
-        return jsonify(error=str(e)), 500
+    # Decode the base64 image
+    image_data = base64.b64decode(image_data.split(",")[1])
+    image = cv2.imdecode(np.frombuffer(image_data, dtype=np.uint8), cv2.IMREAD_COLOR)
+
+    detected_objects = detect_objects(image)
+    return jsonify(detected_objects=detected_objects)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
