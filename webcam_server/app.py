efb44cd024c1554965c4c075f8a8bc6f00f48c82
Injae Shin <101400543+InJae1213@users.noreply.github.com>
2023-10-19 18:22:01 +0900

    Update app.py

--- webcam_server/app.py
+++ webcam_server/app.py
@@ -3,25 +3,34 @@ from flask_cors import CORS
 import cv2
 import numpy as np
 import base64
+import logging
 
+# Flask 앱 초기화
 app = Flask(__name__)
+# CORS 설정 - 다른 도메인에서의 요청을 허용하기 위함
 CORS(app)
 
-# YOLO Configurations
-YOLO_CFG = "yolov3.cfg"
-YOLO_WEIGHTS = "yolov3.weights"
-YOLO_CLASSES = "coco.names"
-CONFIDENCE_THRESHOLD = 0.7  # Adjusted to 0.7 as in the provided YOLO code
-NMS_THRESHOLD = 0.4
+# 로깅 설정 - 디버그 수준의 로그를 출력
+logging.basicConfig(level=logging.DEBUG)
 
+# YOLO 설정값
+YOLO_CFG = "yolov3.cfg"  # YOLO 구성 파일 경로
+YOLO_WEIGHTS = "yolov3.weights"  # YOLO 가중치 파일 경로
+YOLO_CLASSES = "coco.names"  # YOLO 클래스 이름 파일 경로
+CONFIDENCE_THRESHOLD = 0.7  # 객체를 탐지하기 위한 최소 신뢰도
+NMS_THRESHOLD = 0.4  # 비 최대 억제를 위한 임계값
+
+# YOLO 네트워크 로드
 net = cv2.dnn.readNet(YOLO_WEIGHTS, YOLO_CFG)
 layer_names = net.getLayerNames()
 output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers().flatten()]
 classes = open(YOLO_CLASSES).read().strip().split("\n")
 
+# 이미지 내 객체를 탐지하는 함수
 def detect_objects(image):
     height, width, channels = image.shape
 
+    # 이미지를 YOLO 네트워크 입력 형식에 맞게 변환
     blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
     net.setInput(blob)
     outs = net.forward(output_layers)
@@ -30,6 +39,7 @@ def detect_objects(image):
     confidences = []
     boxes = []
 
+    # 탐지된 객체들을 분석하여 리스트에 추가
     for out in outs:
         for detection in out:
             scores = detection[5:]
@@ -55,18 +65,31 @@ def detect_objects(image):
 
     return detected_objects
 
+# 이미지를 받아 객체를 탐지하는 엔드포인트
 @app.route('/detect', methods=['POST'])
 def detect():
     image_data = request.form.get("image")
     if not image_data:
         return jsonify(error="no image provided"), 400
 
-    # Decode the base64 image
-    image_data = base64.b64decode(image_data.split(",")[1])
+    # 로깅을 통해 image_data의 내용 확인
+    logging.debug(f"Received image_data: {image_data[:100]}...")  # 처음 100자만 출력
+
+    # 예외 처리 추가
+    if "," not in image_data:
+        return jsonify(error="Invalid image data format"), 400
+
+    parts = image_data.split(",")
+    if len(parts) < 2:
+        return jsonify(error="Invalid image data format"), 400
+
+    # base64로 인코딩된 이미지를 디코드
+    image_data = base64.b64decode(parts[1])
     image = cv2.imdecode(np.frombuffer(image_data, dtype=np.uint8), cv2.IMREAD_COLOR)
 
     detected_objects = detect_objects(image)
     return jsonify(detected_objects=detected_objects)
 
+# 메인 실행
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
