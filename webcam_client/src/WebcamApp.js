d9688cb5ac5cfa8e05130da3de8a2ae8a8236103
Injae Shin <101400543+InJae1213@users.noreply.github.com>
2023-10-19 18:52:42 +0900

    Update WebcamApp.js

--- webcam_client/src/WebcamApp.js
+++ webcam_client/src/WebcamApp.js
@@ -1,12 +1,20 @@
 import React, { useRef, useEffect, useCallback, useState } from 'react';
 import Webcam from 'react-webcam';
 
-function WebcamApp({ selectedObject, objectCount, shootingInterval }) {
+// 웹캠 앱 컴포넌트
+function WebcamApp({ selectedObject, objectCount, shootingInterval, detectionMode }) {
+    // 웹캠 참조 설정
     const webcamRef = useRef(null);
+    // 촬영 상태 표시 (빨간색: 촬영 중, 초록색: 촬영 완료)
     const [indicatorColor, setIndicatorColor] = useState('red');
+    // 인터넷 연결 상태 확인
     const [isOnline, setIsOnline] = useState(navigator.onLine);
-    const [detectedCount, setDetectedCount] = useState(0); // 실시간 객체 인식 피드백을 위한 state 추가
+    // 감지된 객체 수
+    const [detectedCount, setDetectedCount] = useState(0);
+    // 서버 요청 진행 상태
+    const [isRequestInProgress, setIsRequestInProgress] = useState(false);
 
+    // 이미지 저장 함수
     const saveImageFunction = useCallback(() => {
         const link = document.createElement('a');
         link.href = webcamRef.current.getScreenshot();
@@ -14,25 +22,40 @@ function WebcamApp({ selectedObject, objectCount, shootingInterval }) {
         link.click();
     }, [webcamRef]);
 
+    // 서버에 이미지 전송 및 객체 감지 함수
     const sendImageToServer = useCallback(async () => {
-        if (!webcamRef.current) {
+        if (!webcamRef.current || isRequestInProgress) {
             return;
         }
 
         const imageSrc = webcamRef.current.getScreenshot();
+        if (!imageSrc) {
+            return;
+        }
+
+        setIsRequestInProgress(true);
+
         const formData = new FormData();
         formData.append('image', imageSrc);
 
         try {
-            let response = await fetch("http://localhost:5000/detect", {
+            let response = await fetch("http://192.168.0.5:5000/detect", {
                 method: "POST",
                 body: formData
             });
             let data = await response.json();
             const detectedObjects = data.detected_objects;
             const objectOccurrences = detectedObjects.filter(obj => obj === selectedObject).length;
-            setDetectedCount(objectOccurrences); // 실시간 객체 인식 피드백 업데이트
-            if (objectOccurrences >= objectCount) {
+            setDetectedCount(objectOccurrences);
+
+            // 감지 모드에 따른 이미지 저장 조건
+            if (detectionMode === "above" && objectOccurrences >= objectCount) {
+                saveImageFunction();
+                setIndicatorColor('green');
+            } else if (detectionMode === "below" && objectOccurrences <= objectCount) {
+                saveImageFunction();
+                setIndicatorColor('green');
+            } else if (detectionMode === "fix" && objectOccurrences === objectCount) {
                 saveImageFunction();
                 setIndicatorColor('green');
             } else {
@@ -40,9 +63,12 @@ function WebcamApp({ selectedObject, objectCount, shootingInterval }) {
             }
         } catch (error) {
             console.error("Error sending image to server: ", error);
+        } finally {
+            setIsRequestInProgress(false);
         }
-    }, [selectedObject, objectCount, saveImageFunction]);
+    }, [selectedObject, objectCount, detectionMode, saveImageFunction]);
 
+    // 촬영 간격에 따른 이미지 전송 간격 설정
     useEffect(() => {
         const interval = setInterval(() => {
             sendImageToServer();
@@ -51,6 +77,7 @@ function WebcamApp({ selectedObject, objectCount, shootingInterval }) {
         return () => clearInterval(interval);
     }, [shootingInterval, sendImageToServer]);
 
+    // 인터넷 연결 상태 감지 이벤트 설정
     useEffect(() => {
         window.addEventListener('online', () => setIsOnline(true));
         window.addEventListener('offline', () => setIsOnline(false));
@@ -70,7 +97,7 @@ function WebcamApp({ selectedObject, objectCount, shootingInterval }) {
             />
             <div style={{ position: 'absolute', top: 10, right: 10, width: 30, height: 30, borderRadius: '50%', backgroundColor: indicatorColor }}></div>
             {!isOnline && <p style={{ color: 'red', textAlign: 'center' }}>No internet connection</p>}
-            <p style={{ textAlign: 'center' }}>Detected {selectedObject}s: {detectedCount}</p> {/* 실시간 객체 인식 피드백 표시 */}
+            <p style={{ textAlign: 'center' }}>Detected {selectedObject}s: {detectedCount}</p>
         </div>
     );
 }
